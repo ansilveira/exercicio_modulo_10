acba700205354bedf6d40d68203c36b309a1ed3a
ansilveira <alissonns@hotmail.com>
2024-09-06 16:48:37 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,47 @@
+$('#telefone').mask('(00) 00000-0000', {
+    placeholder: '(DDD) 12345-6789'
+})
+
+$('#cpf').mask('000.000.000-00', {
+    placeholder: '123.456.789-00'
+})
+
+$('#cep').mask('00000-000', {
+    placeholder: '01234-567'
+})
+
+$('form').validate({
+    rules: {
+        nome: {
+            required: true
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        endereco: {
+            required: true
+        },
+        cep: {
+            required: true
+        },
+        cpf: {
+            required: true,
+        }
+    },
+    messages: {
+        nome: 'Por favor, insira seu nome'
+    },
+    submitHandler: function(form) {
+        console.log(form)
+    },
+    invalidHandler: function(evento, validator) {
+        let camposIncorretos = validador.numberOfInvalids();
+        if (camposIncorretos) {
+            alert(`Existem ${camposIncorretos} campos incorretos`)
+        }
+    }
+})
